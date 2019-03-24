ed778d14354370935b6a4aa6013987a892d832f6
Steffan Donal <steffan@sdonalcreative.uk>
2019-03-24 23:40:38 +0100

    Add public properties exposing mod status.

--- Plugin.cs
+++ Plugin.cs
@@ -18,6 +18,28 @@ namespace Claws
         public static readonly string Name = Assembly.GetCustomAttribute<AssemblyTitleAttribute>()?.Title;
         public static readonly string Version = Assembly.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version;
 
+        /// <summary>
+        /// True if the mod is enabled in-game.
+        /// </summary>
+        public static bool IsEnabled
+        {
+            get => _isEnabled;
+            internal set
+            {
+                if (value == _isEnabled) return;
+
+                _isEnabled = value;
+                IsEnabledChanged?.Invoke(typeof(Plugin), EventArgs.Empty);
+            }
+        }
+        static bool _isEnabled;
+
+        /// <summary>
+        /// Invoked whenever the plugin's enabled state changes.
+        /// </summary>
+        public static event EventHandler IsEnabledChanged;
+
+
         void IPlugin.OnApplicationStart()
         {
             Log($"v{Version} loaded!");
