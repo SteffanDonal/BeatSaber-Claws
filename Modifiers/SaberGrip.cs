ac48f01cd5d73cb75ab9e7592d0d37af023fd537
Steffan Donal <steffan@sdonalcreative.uk>
2022-02-19 12:21:09 +0000

    Clear base game offsets when claws are enabled.

--- Modifiers/SaberGrip.cs
+++ Modifiers/SaberGrip.cs
@@ -8,10 +8,22 @@ namespace Claws.Modifiers
     {
         internal static bool IsInGame { get; set; }
 
-        internal static void AdjustControllerTransform(
-            IVRPlatformHelper platformHelper,
-            XRNode node, Transform transform
-        )
+        /// <summary>
+        /// When the plugin is enabled, clears player offset preferences when ingame.
+        /// </summary>
+        internal static void ClearBaseGameOffsets(ref Vector3 position, ref Vector3 rotation)
+        {
+            if (!Plugin.IsEnabled) return;
+            if (!IsInGame) return;
+
+            position = Vector3.zero;
+            rotation = Vector3.zero;
+        }
+
+        /// <summary>
+        /// When the plugin is enabled, applies current controller offsets when ingame.
+        /// </summary>
+        internal static void AdjustControllerTransform(XRNode node, Transform transform)
         {
             if (!Plugin.IsEnabled) return;
             if (!IsInGame) return;
@@ -35,32 +47,50 @@ namespace Claws.Modifiers
     [HarmonyPatch(nameof(IVRPlatformHelper.AdjustControllerTransform))]
     internal class SaberGripDevicelessControllerPatch
     {
+        static void Prefix(
+            DevicelessVRHelper __instance,
+            XRNode node, Transform transform,
+            ref Vector3 position, ref Vector3 rotation
+        ) => SaberGrip.ClearBaseGameOffsets(ref position, ref rotation);
+
         static void Postfix(
             DevicelessVRHelper __instance,
             XRNode node, Transform transform,
             Vector3 position, Vector3 rotation
-        ) => SaberGrip.AdjustControllerTransform(__instance, node, transform);
+        ) => SaberGrip.AdjustControllerTransform(node, transform);
     }
 
     [HarmonyPatch(typeof(OculusVRHelper))]
     [HarmonyPatch(nameof(IVRPlatformHelper.AdjustControllerTransform))]
     internal class SaberGripOculusControllerPatch
     {
+        static void Prefix(
+            OculusVRHelper __instance,
+            XRNode node, Transform transform,
+            ref Vector3 position, ref Vector3 rotation
+        ) => SaberGrip.ClearBaseGameOffsets(ref position, ref rotation);
+
         static void Postfix(
             OculusVRHelper __instance,
             XRNode node, Transform transform,
             Vector3 position, Vector3 rotation
-        ) => SaberGrip.AdjustControllerTransform(__instance, node, transform);
+        ) => SaberGrip.AdjustControllerTransform(node, transform);
     }
 
     [HarmonyPatch(typeof(OpenVRHelper))]
     [HarmonyPatch(nameof(IVRPlatformHelper.AdjustControllerTransform))]
     internal class SaberGripOpenVRControllerPatch
     {
+        static void Prefix(
+            OpenVRHelper __instance,
+            XRNode node, Transform transform,
+            ref Vector3 position, ref Vector3 rotation
+        ) => SaberGrip.ClearBaseGameOffsets(ref position, ref rotation);
+
         static void Postfix(
             OpenVRHelper __instance,
             XRNode node, Transform transform,
             Vector3 position, Vector3 rotation
-        ) => SaberGrip.AdjustControllerTransform(__instance, node, transform);
+        ) => SaberGrip.AdjustControllerTransform(node, transform);
     }
 }
