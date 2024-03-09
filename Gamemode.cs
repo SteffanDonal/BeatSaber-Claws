ec4a5f9f8b4cb4808a254416ced4ef93d36d64c1
SteffanDonal <steffandonal@users.noreply.github.com>
2024-03-09 16:43:05 +0000

    Remove BS_Utils and SongCore dependencies. They were largely ineffectual anyway.

--- Gamemode.cs
+++ Gamemode.cs
@@ -1,8 +1,5 @@
 ﻿using BeatSaberMarkupLanguage.GameplaySetup;
-using BS_Utils.Utilities;
 using Claws.Views;
-using SongCore;
-using System;
 
 namespace Claws
 {
@@ -13,40 +10,12 @@ namespace Claws
         internal Gamemode()
         {
             _gamemodeSettingsView = new GamemodeSettingsViewController();
-            _gamemodeSettingsView.IsEnabledChanged += OnGamemodeToggled;
 
             GameplaySetup.instance.AddTab(
                 "Claws",
                 GamemodeSettingsViewController.Resource,
                 _gamemodeSettingsView
             );
-
-            BSEvents.OnLoad();
-            BSEvents.gameSceneActive += OnGameActivated;
-            BSEvents.menuSceneActive += OnMenuActivated;
-        }
-
-        void OnMenuActivated()
-        {
-            UpdateCapability();
-        }
-
-        void OnGameActivated()
-        {
-            Preferences.Invalidate();
-        }
-
-        void OnGamemodeToggled(object sender, EventArgs e)
-        {
-            UpdateCapability();
-        }
-
-        void UpdateCapability()
-        {
-            if (Plugin.IsEnabled)
-                Collections.RegisterCapability(Plugin.CapabilityName);
-            else
-                Collections.DeregisterizeCapability(Plugin.CapabilityName);
         }
     }
 }
